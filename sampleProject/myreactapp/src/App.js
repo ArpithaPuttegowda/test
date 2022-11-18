0982f25fa5995518b6e8c0245cba293a3dcc97d4
Arpitha-P_cdk <arpitha.p@cdk.com>
2022-11-18 09:01:20 +0530

    unit test cases

--- sampleProject/myreactapp/src/App.js
+++ sampleProject/myreactapp/src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, Component, PureComponent, useReducer} from "react";
-import {ApplyStylesDynamically} from "./Components/ApplyStylesDynamically";
-import ArithMetic from "./Components/ArithMetic";
+// import React, {useState, Component, PureComponent, useReducer} from "react";
+// import {ApplyStylesDynamically} from "./Components/ApplyStylesDynamically";
+// import ArithMetic from "./Components/ArithMetic";
 // function App() {
 //   return (
 //     <div>
@@ -13,48 +13,50 @@ import ArithMetic from "./Components/ArithMetic";
 //   );
 // }
 // export default App;
-import Box from "./Components/Box";
-import BoxC from "./Components/BoxC";
-import {ChildToParent} from "./Components/ChildToParent";
-import ChildTOParentC from "./Components/ChildTOParentC";
-import {ControlledComponent} from "./Components/ControlledComponent";
-import Count from "./Components/Count";
-import {CountF} from "./Components/CountF";
-import {ExampleCustomHook} from "./Components/customHook/ExampleCustomHook";
-import ExampleOwnRef from "./Components/ExampleOwnRef";
-import ExampleOwnRefF from "./Components/ExampleOwnRefF";
-import ExceptionHandling from "./Components/ExceptionHandling/ExceptionHandling";
-import X from "./Components/ExceptionHandling/X";
-import {Footer} from "./Components/Footer";
-import {Header} from "./Components/Header";
-import Parent from "./Components/HOC/Parent";
-import {LifeCycleForFunctionalComponent} from "./Components/LifeCycleForFunctionalComponent";
-import {LifeCycleFunctionalComponentWillUnMount} from "./Components/LifeCycleFunctionalComponentWillUnMount";
-import LifeCycleMethodAjax from "./Components/LifeCycleMethodAjax";
-import LifeCycleMethods from "./Components/LifeCycleMethods";
-import ListKeys from "./Components/ListKeys";
-import {ListKeysPlayer} from "./Components/ListKeysPlayer";
-import {ListKeysSelect} from "./Components/ListKeysSelect";
-import {ListKeysTable} from "./Components/ListKeysTable";
-import {Menu} from "./Components/Menu";
-import A from "./Components/myContextF";
-import {ExampleReactMemo} from "./Components/myReactMemo";
-import {Player} from "./Components/Player";
-import PlayerC from "./Components/PlayerC";
-import Example from "./Components/PureComponent";
-import SetStateAsCB from "./Components/SetStateAsCB";
-import SetStateAsObjCb from "./Components/SetStateAsObjCb";
-import {TakingDataFUseRef} from "./Components/TakingDataFUseRef";
-import UnMountingPhase from "./Components/UnMountingPhase";
-import "./App.css";
-import {SampleUseReducerHook} from "./Components/GlobalStateUseReducer/SampleUseReducerHook";
-import {reducer} from "./Components/GlobalStateUseReducer/reducer";
-import {initData} from "./Components/GlobalStateUseReducer/initData";
-import {ctx} from "./Components/GlobalStateUseReducer/myctx";
-import {AxiosAjax} from "./Components/AxiosAjax";
-import {ExampleUseCallback} from "./Components/ExampleUseCallback";
-import {ExampleForUseMemo} from "./Components/ExampleForUseMemo";
+// import Box from "./Components/Box";
+// import BoxC from "./Components/BoxC";
+// import {ChildToParent} from "./Components/ChildToParent";
+// import ChildTOParentC from "./Components/ChildTOParentC";
+// import {ControlledComponent} from "./Components/ControlledComponent";
+// import Count from "./Components/Count";
+// import {CountF} from "./Components/CountF";
+// import {ExampleCustomHook} from "./Components/customHook/ExampleCustomHook";
+// import ExampleOwnRef from "./Components/ExampleOwnRef";
+// import ExampleOwnRefF from "./Components/ExampleOwnRefF";
+// import ExceptionHandling from "./Components/ExceptionHandling/ExceptionHandling";
+// import X from "./Components/ExceptionHandling/X";
+// import {Footer} from "./Components/Footer";
+// import {Header} from "./Components/Header";
+// import Parent from "./Components/HOC/Parent";
+// import {LifeCycleForFunctionalComponent} from "./Components/LifeCycleForFunctionalComponent";
+// import {LifeCycleFunctionalComponentWillUnMount} from "./Components/LifeCycleFunctionalComponentWillUnMount";
+// import LifeCycleMethodAjax from "./Components/LifeCycleMethodAjax";
+// import LifeCycleMethods from "./Components/LifeCycleMethods";
+// import ListKeys from "./Components/ListKeys";
+// import {ListKeysPlayer} from "./Components/ListKeysPlayer";
+// import {ListKeysSelect} from "./Components/ListKeysSelect";
+// import {ListKeysTable} from "./Components/ListKeysTable";
+// import {Menu} from "./Components/Menu";
+// import A from "./Components/myContextF";
+// import {ExampleReactMemo} from "./Components/myReactMemo";
+// import {Player} from "./Components/Player";
+// import PlayerC from "./Components/PlayerC";
+// import Example from "./Components/PureComponent";
+// import SetStateAsCB from "./Components/SetStateAsCB";
+// import SetStateAsObjCb from "./Components/SetStateAsObjCb";
+// import {TakingDataFUseRef} from "./Components/TakingDataFUseRef";
+// import UnMountingPhase from "./Components/UnMountingPhase";
+// import "./App.css";
+// import {SampleUseReducerHook} from "./Components/GlobalStateUseReducer/SampleUseReducerHook";
+// import {reducer} from "./Components/GlobalStateUseReducer/reducer";
+// import {initData} from "./Components/GlobalStateUseReducer/initData";
+// import {ctx} from "./Components/GlobalStateUseReducer/myctx";
+// import {AxiosAjax} from "./Components/AxiosAjax";
+// import {ExampleUseCallback} from "./Components/ExampleUseCallback";
+// import {ExampleForUseMemo} from "./Components/ExampleForUseMemo";
 // import {A} from "./Components/myContextF";
+
+import {useRef, useState} from "react";
 
 // const App = () => {
 //   return (
@@ -240,20 +242,49 @@ import {ExampleForUseMemo} from "./Components/ExampleForUseMemo";
 //     );
 //   }
 // }
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, initData);
+// const App = () => {
+//   const [state, dispatch] = useReducer(reducer, initData);
+//   return (
+//     <div>
+//       <ctx.Provider value={{state, dispatch}}>
+//         <SampleUseReducerHook />
+//         <ChildTOParentC />
+//         <AxiosAjax />
+//         <h1>USECALLBACK</h1>
+//         <ExampleUseCallback />
+//         <h1>USEMEMO</h1>
+//         <ExampleForUseMemo />
+//       </ctx.Provider>
+//     </div>
+//   );
+// };
+// export default App;
+
+export const App = () => {
+  const [data, setData] = useState();
+  const [count, setCount] = useState(0);
+  const inputRef = useRef(null);
+  const handleInc = () => {
+    let inputValue = inputRef.current.value;
+    setCount(count + Number(inputValue));
+  };
+
+  const handleDec = () => {
+    let inputValue = inputRef.current.value;
+    setCount(count - Number(inputValue));
+  };
   return (
-    <div>
-      <ctx.Provider value={{state, dispatch}}>
-        <SampleUseReducerHook />
-        <ChildTOParentC />
-        <AxiosAjax />
-        <h1>USECALLBACK</h1>
-        <ExampleUseCallback />
-        <h1>USEMEMO</h1>
-        <ExampleForUseMemo />
-      </ctx.Provider>
-    </div>
+    <>
+      <h1>Count</h1>
+      <h2>{data}</h2>
+      <button onClick={handleInc}>+</button>
+      <input
+        ref={inputRef}
+        type="number"
+        onChange={(e) => setData(e.target.value)}
+      />
+      <button onClick={handleDec}>-</button>
+      <h2>{count}</h2>
+    </>
   );
 };
-export default App;
