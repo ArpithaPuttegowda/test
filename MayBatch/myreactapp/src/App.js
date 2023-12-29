718fde50e2369a22c744bacd17426d4ebd5e8528
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-12-29 09:05:28 +0530

    grid

--- MayBatch/myreactapp/src/App.js
+++ MayBatch/myreactapp/src/App.js
@@ -1,15 +1,19 @@
 
 import React from 'react'
+import { createPortal } from 'react-dom'
 import { FetchData } from './Components/FetchData'
 import { FetchDataClick } from './Components/FetchDataClick'
 import MyComponent from './Components/InsertTheData'
+import {ModalComp} from './Components/ModalComp'
 
 export const App = () => {
   return (
-    <div>App
-      <FetchData/>
+    <div>
+      {/* <FetchData/>
       <FetchDataClick/>
-      <MyComponent/>
+      <MyComponent/> */}
+      <h1>App</h1>
+      {createPortal(<ModalComp/>,document.getElementById("modal-root"))}
     </div>
   )
 }
