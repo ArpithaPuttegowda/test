0833d243a4b734844b725463b6887ff55a27f20f
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-12-30 17:07:13 +0530

    cypress

--- MayBatch/cypressE2E/cypress/e2e/example.cy.js
+++ MayBatch/cypressE2E/cypress/e2e/example.cy.js
@@ -1,13 +1,40 @@
-// openai_chat_spec.js
-
-describe('OpenAI Chat Application Tests', () => {
-    beforeEach(() => {
-      // Visit the OpenAI Chat application before each test
-      cy.visit('https://chat.openai.com/');
-    });
-  
-    it("should show some text",()=>{
-        cy.get('[data-testid="login-button"]').click()
-    })
+describe('Counter App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000'); // Update with the actual URL of your app
   });
-  
+
+  it('should display the heading', () => {
+    cy.get('[data-testid=heading]').should('have.text', 'Counter');
+  });
+
+  it('should increment the count when the + button is clicked', () => {
+    cy.get('[data-testid="input-data"]').type("1")
+    cy.get('[data-testid="inc-btn"]').click();
+    cy.get('[data-testid="count"]').should('have.text', '1');
+  });
+
+  it('should decrement the count when the - button is clicked', () => {
+    cy.get('[data-testid="input-data"]').type("1")
+    cy.get('[data-testid="dec-btn"]').click();
+    cy.get('[data-testid="count"]').should('have.text', '-1');
+  });
+
+  it('should update the count based on input value', () => {
+    cy.get('[data-testid="input-data"]').type('5');
+    cy.get('[data-testid="inc-btn"]').click();
+    cy.get('[data-testid="count"]').should('have.text', '5');
+  });
+
+  it('should handle negative input values when decrementing', () => {
+    cy.get('[data-testid="input-data"]').type('3');
+    cy.get('[data-testid="dec-btn"]').click();
+    cy.get('[data-testid="dec-btn"]').click();
+    cy.get('[data-testid="count"]').should('have.text', '-6');
+  });
+
+  it('should handle floating-point input values when incrementing', () => {
+    cy.get('[data-testid="input-data"]').type('2.5');
+    cy.get('[data-testid="inc-btn"]').click();
+    cy.get('[data-testid="count"]').should('have.text', '2.5');
+  });
+});
---
+++ MayBatch/cypressE2E/reactapp/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export const App = () => {
+  const [count,setCount]=useState(0)
+  const [input,setInput]=useState("")
+  const handleDec=()=>{
+    setCount(count-Number(input))
+  }
+  const handleInc=()=>{
+    setCount(count+Number(input))
+  }
+  return (
+    <div>
+      <h1 data-testid="heading">Counter</h1>
+      <button data-testid="dec-btn" onClick={handleDec}>-</button>
+      <input value={input} data-testid="input-data" onChange={(e)=>setInput(e.target.value)}/>
+      <button data-testid="inc-btn" onClick={handleInc}>+</button>
+      <p>
+        <span data-testid="count-text">Count</span><br/>
+        <b data-testid="count">{count}</b>
+      </p>
+    </div>
+  )
+}
+
