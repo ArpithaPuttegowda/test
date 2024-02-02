621ec580db4702bf6264ce4ca8c10414b55c23ea
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2024-02-02 10:25:29 +0530

    ptoc

--- 13.12.2023Batch/React/myfirstreactapp/src/App.js
+++ 13.12.2023Batch/React/myfirstreactapp/src/App.js
@@ -1,6 +1,15 @@
-const App=()=>{
-    return <h1>App component</h1>
-}
+import PlayerName from "./Components/Child";
 
+const App = () => {
+  const loc = "Maharastra";
+  const runs=20
+  return (
+    <div>
+      <h1>App component</h1>
+      <PlayerName myLoc={loc} r={runs} />
+    </div>
+  );
+};
 
 export default App;
+
---
+++ 13.12.2023Batch/React/myfirstreactapp/src/Components/Child.js
@@ -0,0 +1,6 @@
+
+const PlayerName=({myLoc,r})=>{
+    return <div>Data coming from Parent::::::{myLoc}....{r}</div>
+}
+
+export default PlayerName
